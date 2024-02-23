49ec6ff3b19781c8be51984faedf7cfbecfdcf74
trungvu <nam24620@gmail.com>
2024-02-23 09:23:00 +0700

    rename route and edit page content

--- component/Navigation.tsx
+++ component/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
     return (
         <nav className="flex gap-5 divide-x" style={{gap: "5px"}}>
             <Link className="block text-slate-300" href="/">Home</Link>
-            <Link className="block text-slate-300" href="/portfolio">Learning</Link>
+            <Link className="block text-slate-300" href="/learning">Learning</Link>
             <Link className="block text-slate-300" href="/about">About</Link>
         </nav>
     )
