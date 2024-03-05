2162decd34ecd94eacc213735e234c6d9c352d5b
trungvu <nam24620@gmail.com>
2024-03-05 14:54:30 +0700

    delete unnecessery class

--- component/Navigation.tsx
+++ component/Navigation.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function Navigation() {
     return (
-        <nav className="flex gap-5 divide-x" style={{gap: "5px"}}>
-            <Link className="block text-slate-300" href="/">Home</Link>
-            <Link className="block text-slate-300" href="/learning">Learning</Link>
-            <Link className="block text-slate-300" href="/about">About</Link>
+        <nav className="flex gap-5">
+            <Link className="block font-bold" style={{color: '#000',}} href="/">Home</Link>
+            <Link className="block font-bold" style={{color: '#000',}} href="/learning">Learning</Link>
+            <Link className="block font-bold" style={{color: '#000',}} href="/about">About</Link>
         </nav>
     )
 }
