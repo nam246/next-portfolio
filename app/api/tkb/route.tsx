2e0789f104e9d0152462da03fafbde6122cf55c7
nam246 <nam24620@gmail.com>
2025-03-28 09:17:35 +0700

    fix build err

--- app/api/tkb/route.tsx
+++ app/api/tkb/route.tsx
@@ -14,7 +14,7 @@ export async function GET() {
             headers: { "Content-Type": "application/json" }
         });
     } catch (error) {
-        return new Response(JSON.stringify({ error: "Không thể đọc file JSON" }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
 
@@ -27,6 +27,19 @@ export async function GET() {
 //         return new Response(JSON.stringify({ error: "Không thể ghi vào file JSON" }), { status: 500 });
 //     }
 // }
+
+export async function POST(req: Request): Promise<Response> {
+    const dataFs = await fs.readFile(filePath, "utf-8");
+
+    const data = await JSON.parse(dataFs);
+    const body = await req.json();
+
+    data[body.index].subjects.push(body.data);
+
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2), "utf-8");
+
+    return new Response(JSON.stringify({ message: "Lưu dữ liệu thành công!" }), { status: 200 });
+}
 
 export async function DELETE(req: Request) {
 
@@ -41,7 +54,7 @@ export async function DELETE(req: Request) {
         return new Response(JSON.stringify({ message: "Lưu dữ liệu thành công!" }), { status: 200 });
 
     } catch (error) {
-        return new Response(JSON.stringify({ error: "Không thể ghi vào file JSON" }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
 
@@ -60,32 +73,27 @@ export async function PATCH(req: Request) {
         return new Response(JSON.stringify({ message: "Lưu dữ liệu thành công!" }), { status: 200 });
 
     } catch (error) {
-        return new Response(JSON.stringify({ error: "Không thể ghi vào file JSON" }), { status: 500 });
+        return new Response(JSON.stringify({ error: error }), { status: 500 });
     }
 }
 
-export async function create(formData: FormData): Promise<any> {
+// export default async function create(formData: FormData): Promise<void | never> {
 
-    const data = await fs.readFile(filePath, "utf-8");
+//     const data = await fs.readFile(filePath, "utf-8");
 
-    const newData = await JSON.parse(data);
+//     const newData = await JSON.parse(data);
 
-    const index: any = formData.get("index");
-    if (index != null) {
-        newData[index].subjects.push({
-            "maMonHoc": formData.get("maMonHoc"),
-            "tenMonHoc": formData.get("tenMonHoc"),
-            "phongHoc": formData.get("phongHoc"),
-            "thoiGianBatDau": formData.get("thoiGianBatDau"),
-            "thoiGianKetThuc": formData.get("thoiGianKetThuc")
-        })
-    }
+//     const index = formData.get("index");
+//     if (typeof index === "string" && !isNaN(Number(index))) {
+//         const numericIndex = Number(index);
+//         newData[numericIndex].subjects.push({
+//             "maMonHoc": formData.get("maMonHoc"),
+//             "tenMonHoc": formData.get("tenMonHoc"),
+//             "phongHoc": formData.get("phongHoc"),
+//             "thoiGianBatDau": formData.get("thoiGianBatDau"),
+//             "thoiGianKetThuc": formData.get("thoiGianKetThuc")
+//         })
+//     }
 
-
-    await fs.writeFile(filePath, JSON.stringify(newData, null, 2), "utf-8");
-
-    return {
-        message: "created succesfully."
-    }
-}
-
+//     await fs.writeFile(filePath, JSON.stringify(newData, null, 2), "utf-8");
+// }
--- app/tkb/page.tsx
+++ app/tkb/page.tsx
@@ -15,8 +15,6 @@ type TkbType = {
 
 import { useState, useEffect } from "react";
 
-import { create } from "@/app/api/tkb/route";
-
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Button } from "@/components/ui/button";
@@ -33,23 +31,21 @@ import { PopoverContent } from "@/components/ui/popover";
 
 
 export default function Tkb() {
-
     const [data, setData] = useState<TkbType[]>([])
 
-    const [index, setIndex] = useState<number>();
     const [maMonHoc, setMaMonHoc] = useState<string>("");
     const [monHoc, setMonHoc] = useState<string>("");
     const [phongHoc, setPhongHoc] = useState<string>("");
     const [thoiGianBatDau, setThoiGianBatDau] = useState<Date>();
     const [thoiGianKetThuc, setThoiGianKetThuc] = useState<Date>();
 
-    const formData = new FormData();
-    formData.append("index", index as any);
-    formData.append("maMonHoc", maMonHoc as string);
-    formData.append("tenMonHoc", monHoc as string);
-    formData.append("phongHoc", phongHoc as string);
-    formData.append("thoiGianBatDau", thoiGianBatDau as any);
-    formData.append("thoiGianKetThuc", thoiGianKetThuc as any);
+    const reqData: Subject = {
+        maMonHoc: maMonHoc,
+        tenMonHoc: monHoc,
+        phongHoc: phongHoc,
+        thoiGianBatDau: thoiGianBatDau || new Date(),
+        thoiGianKetThuc: thoiGianKetThuc || new Date()
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -63,6 +59,26 @@ export default function Tkb() {
         }
         fetchData();
     }, [])
+
+    const postHandle = async (index: number) => {
+        const data = await fetch("api/tkb", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                index: index,
+                data: reqData
+            }),
+        })
+
+        const res = await data.json();
+
+        if (res.status) {
+            console.log("created!");
+        }
+    }
 
     const deleteHandle = async (index: number, arrIndex: number) => {
         const res = await fetch("/api/tkb", {
@@ -90,11 +106,11 @@ export default function Tkb() {
         }
     }
 
-    function dateCalculate(e: Date) {
+    function dateCalculate(d: Date) {
         const currDate = new Date();
-        const endDate = new Date(e);
+        const endDate = new Date(d);
 
-        const diff: any = Number(endDate) - Number(currDate);
+        const diff = Number(endDate) - Number(currDate);
         const diffInHour = (diff / (1000 * 60 * 60)) / 24;
 
         return diffInHour;
@@ -129,35 +145,60 @@ export default function Tkb() {
                                     <p>Kết thúc: <b>{String(subject.thoiGianKetThuc)}</b></p>
                                     <p>{countDown(subject, index, i)}</p>
                                 </div>
-                                <Button onClick={() => deleteHandle(index, i)} className="bg-red-500">Xóa</Button>
-                                <Button onClick={() => patchHandle(index, i)} className="mx-2 bg-yellow-500">Sửa</Button>
+                                <Button type="button" onClick={() => deleteHandle(index, i)} className="bg-red-500">Xóa</Button>
+                                <Button type="button" onClick={() => patchHandle(index, i)} className="mx-2 bg-yellow-500">Sửa</Button>
                             </CardContent>
                         ))}
                         <CardFooter>
                             <Popover>
                                 <PopoverTrigger className="bg-blue-200 rounded-lg p-2">Thêm môn học</PopoverTrigger>
                                 <PopoverContent>
-                                    <form action={create} className="flex flex-col">
+                                    <form onSubmit={() => postHandle(index)} className="flex flex-col">
                                         <Input type="hidden" name="index" value={index} />
                                         <div>
                                             <Label htmlFor="maMonHoc">Tên môn học</Label>
-                                            <Input type="text" name="maMonHoc" placeholder="Nhập mã môn học" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMaMonHoc(e.target.value)} />
+                                            <Input
+                                                type="text"
+                                                name="maMonHoc"
+                                                placeholder="Nhập mã môn học"
+                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMaMonHoc(e.target.value)}
+                                            />
                                         </div>
                                         <div>
                                             <Label htmlFor="tenMonHoc">Tên môn học</Label>
-                                            <Input type="text" name="tenMonHoc" placeholder="Nhập tên môn học" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMonHoc(e.target.value)} />
+                                            <Input
+                                                type="text"
+                                                name="tenMonHoc"
+                                                placeholder="Nhập tên môn học"
+                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setMonHoc(e.target.value)}
+                                            />
                                         </div>
                                         <div>
                                             <Label htmlFor="phongHoc">Phòng học</Label>
-                                            <Input type="text" name="phongHoc" placeholder="Nhập phòng học" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhongHoc(e.target.value)} />
+                                            <Input
+                                                type="text"
+                                                name="phongHoc"
+                                                placeholder="Nhập phòng học"
+                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPhongHoc(e.target.value)}
+                                            />
                                         </div>
                                         <div>
                                             <Label htmlFor="thoiGianBatDau">Thời gian bắt đầu</Label>
-                                            <Input type="date" name="thoiGianBatDau" placeholder="Nhập thời gian bắt đầu" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setThoiGianBatDau(new Date(e.target.value))} />
+                                            <Input
+                                                type="date"
+                                                name="thoiGianBatDau"
+                                                placeholder="Nhập thời gian bắt đầu"
+                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setThoiGianBatDau(new Date(e.target.value))}
+                                            />
                                         </div>
                                         <div>
                                             <Label htmlFor="thoiGianKetThuc">Thời gian kết thúc</Label>
-                                            <Input type="date" name="thoiGianKetThuc" placeholder="Nhập thời gian kết thúc" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setThoiGianKetThuc(new Date(e.target.value))} />
+                                            <Input
+                                                type="date"
+                                                name="thoiGianKetThuc"
+                                                placeholder="Nhập thời gian kết thúc"
+                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setThoiGianKetThuc(new Date(e.target.value))}
+                                            />
                                         </div>
                                         <Button type="submit">Submit</Button>
                                     </form>
