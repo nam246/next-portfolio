3725aa5a15d1350a64736d9ce14436d19942686b
nam246 <nam24620@gmail.com>
2025-07-19 16:31:40 +0700

    add some components

--- src/components/layout/HeroCarousel.tsx
+++ src/components/layout/HeroCarousel.tsx
@@ -1,30 +1,52 @@
-import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import { AspectRatio } from "../ui/aspect-ratio";
 
+const images = [
+	{
+		url: "/carousel/buddy-an-ivaWTRbSH1I-unsplash.jpg",
+	},
+	{
+		url: "/carousel/jeff-james-2NCsNfCqFec-unsplash.jpg",
+	},
+	{
+		url: "/carousel/kelly-sikkema-gRZynBXSdz4-unsplash.jpg",
+	},
+	{
+		url: "/carousel/nikita-pishchugin-y2lZI81BGk0-unsplash.jpg",
+	},
+	{
+		url: "/carousel/tuaans-3cwIyLwYfzc-unsplash.jpg",
+	},
+];
 export function HeroCarousel() {
-  return (
-    <Carousel className="w-full ">
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+	return (
+		<Carousel className="w-full ">
+			<CarouselContent>
+				{images.map((image, index) => (
+					<CarouselItem key={index}>
+						<AspectRatio ratio={1 / 1}>
+							<Image
+								className="rounded-md"
+								src={image.url}
+                loading="lazy"
+								alt={`carousel ${index}`}
+								fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+							/>
+						</AspectRatio>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
+	);
 }
